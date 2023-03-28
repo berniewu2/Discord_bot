9960f26093fe28ed4d796f51376cfc8249a1a3a5
berniewu2 <129116216+berniewu2@users.noreply.github.com>
2023-03-27 21:53:08 -0700

    Update screenshot.py
    add probuild case

--- screenshot.py
+++ screenshot.py
@@ -7,12 +7,17 @@ options = Options()
 options.headless = True
 
 
-def screenshot(champion, role):
+def screenshot(champion, role, probuild = False):
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(f'https://www.op.gg/champions/{champion}/{role}/build?region=global&tier=platinum_plus')
+    
+    if probuild:
+        driver.get(f'https://probuildstats.com/champion/{champion}?league=lpl&role={role}')
 
-    driver.execute_script("window.scrollTo(0,200)")
+    if not probuild:
+        driver.execute_script("window.scrollTo(0,200)")
+
     driver.set_window_size(1200,1500) # May need manual adjustment(w,h)                                                                                                                
     driver.save_screenshot("screenshot.png")
     driver.quit()
