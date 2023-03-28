3e317ef339581f216bd869117e33984650841779
berniewu2 <129116216+berniewu2@users.noreply.github.com>
2023-03-27 19:52:50 -0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
 from selenium.webdriver.chrome.options import Options
 
 
@@ -13,7 +12,6 @@ def screenshot(champion, role):
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(f'https://www.op.gg/champions/{champion}/{role}/build?region=global&tier=platinum_plus')
 
-    S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
     driver.execute_script("window.scrollTo(0,200)")
     driver.set_window_size(1200,1500) # May need manual adjustment(w,h)                                                                                                                
     driver.save_screenshot("screenshot.png")
