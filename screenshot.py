dac6317a8658724ce95200a7c5f66f297ebabd33
berniewu2 <129116216+berniewu2@users.noreply.github.com>
2023-03-28 19:24:51 -0700

    Update screenshot.py
    add screenshot_name

--- screenshot.py
+++ screenshot.py
@@ -9,6 +9,21 @@ options = Options()
 options.headless = True
 
 
+def screenshot_name(name):
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    driver.get(f'https://www.op.gg/summoners/na/{name}')
+
+    driver.execute_script("window.scrollTo(0,200)")
+
+    driver.set_window_size(1200,1500) # May need manual adjustment(w,h)                                                                                                                
+    driver.save_screenshot("screenshot.png")
+    driver.quit()
+    print("end...")
+    return
+
+
 def screenshot(champion, role):
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
