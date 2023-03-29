13bdab60e958e3056115ce2e46df04ec19b16c93
berniewu2 <129116216+berniewu2@users.noreply.github.com>
2023-03-28 18:52:17 -0700

    Update screenshot.py
    separate opgg and probuild

--- screenshot.py
+++ screenshot.py
@@ -1,25 +1,36 @@
 from selenium import webdriver
-from time import sleep
+import time
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 options = Options()
 options.headless = True
 
 
-def screenshot(champion, role, probuild = False):
+def screenshot(champion, role):
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.get(f'https://www.op.gg/champions/{champion}/{role}/build?region=global&tier=platinum_plus')
-    sleep(1)
-    
-    if probuild:
-        driver.get(f'https://probuildstats.com/champion/{champion}?league=lpl&role={role}')
-        sleep(1)
 
-    if not probuild:
-        driver.execute_script("window.scrollTo(0,200)")
+    driver.get(f'https://www.op.gg/champions/{champion}/{role}/build?region=global&tier=platinum_plus')
+
+
+    driver.execute_script("window.scrollTo(0,200)")
+
+    driver.set_window_size(1200,1500) # May need manual adjustment(w,h)                                                                                                                
+    driver.save_screenshot("screenshot.png")
+    driver.quit()
+    print("end...")
+    return
+
+def screenshot_pro(champion, role, lpl):
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    if lpl:
+        driver.get(f'https://probuildstats.com/champion/{champion}?league=lpl&role={role}')
+    else:
+        driver.get(f'https://probuildstats.com/champion/{champion}?role={role}')
 
     driver.set_window_size(1200,1500) # May need manual adjustment(w,h)                                                                                                                
     driver.save_screenshot("screenshot.png")
