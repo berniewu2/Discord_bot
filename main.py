b21b1bb5f77e0ea4b2beffa8d518342d1e576045
Bernie Wu <129116216+berniewu2@users.noreply.github.com>
2023-04-02 16:42:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ async def guess(message, guess):
   if message.channel.id not in HangmanGame.games:
     await message.channel.send('START FIRST')
     return
-  if (len(guess)>len(HangmanGame.games[message.channel.id].word)) || (guess.isalpha()):
+  if (len(guess)>len(HangmanGame.games[message.channel.id].word)) | (guess.isalpha()):
     await message.channel.send('Didn\'t ur parents teach u how to play hangman?')
     return
   if len(guess)==1:
